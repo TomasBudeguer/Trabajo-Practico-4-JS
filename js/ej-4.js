44ae876dea6d4106b01ab762e7532e71562443a4
Tomas <tomibudeguer20@gmail.com>
2022-07-27 10:44:49 -0300

    hice el ejercicio 4

---
+++ js/ej-4.js
@@ -0,0 +1,26 @@
+class Producto {
+  constructor(codigo, nombre, precio) {
+    this.codigo = codigo;
+    this.nombre = nombre;
+    this.precio = precio;
+  }
+  imprimirDatos() {
+    document.write(`<ul>
+        <li>Codigo: ${this.codigo}</li>
+        <li>Nombre: ${this.nombre}</li>
+        <li>Precio: $${this.precio}</li>
+        </ul>`);
+  }
+}
+
+let producto1 = new Producto(194428, "Coca-Cola", 230);
+let producto2 = new Producto(123456, "Oreo", 170);
+let producto3 = new Producto(987123, "Papas Fritas Lays", 230);
+
+let productos = [producto1, producto2, producto3];
+console.log(productos);
+
+productos[producto1.imprimirDatos()];
+productos[producto2.imprimirDatos()];
+productos[producto3.imprimirDatos()];
+
